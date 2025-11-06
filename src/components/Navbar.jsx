3cdc0c64b20924aae83250908b1e3284a89cb713
Latech Hub <latechhub@gmail.com>
2025-11-06 14:33:46 +0300

    2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
 
-export default function Navbar(){
+export default function Navbar() {
+  // Add smooth shrink effect on scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector('.header')
+      if (window.scrollY > 50) {
+        header.classList.add('shrink')
+      } else {
+        header.classList.remove('shrink')
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
     <header className="header">
-      <img src={logo} alt="Ajidaik Homes logo" />
+      {/* Logo: Left aligned, large and crisp */}
+      <div className="logo-area">
+        <img
+          src={logo}
+          alt="Ajidaik Homes logo"
+          className="logo"
+          draggable="false"
+        />
+      </div>
+
+      {/* Navigation */}
       <nav className="nav">
         <Link to="/">Home</Link>
         <Link to="/browse">Browse Homes</Link>
@@ -13,7 +37,8 @@ export default function Navbar(){
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
       </nav>
-      {/* Contact info removed for a cleaner, countrywide look */}
     </header>
   )
+}
+
 }
